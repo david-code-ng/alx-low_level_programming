830580990dc554dd528945e43bb31d9dde0a8ccf
David <Davidvill30@gmail.com>
2022-12-07 22:58:28 -0800

    done with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 include <stdio.h>
-
-/**
- *  * main - Entry point
- *   *
- *    * Description: prints "Programming is like building a multilingual puzzle"
- *     *
- *      * Return: Always 0 (Success)
- *       */
-int main(void)
+ /**
+  * main -Entry point
+  *
+  * Description:prints "programming is fun"
+  *
+  * Return Always 0 (success)
+  */
+int main{void}
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+	puts("/"Programming is like building a multilingual puzzle");
+		return (0);
